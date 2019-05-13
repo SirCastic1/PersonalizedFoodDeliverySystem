39139e0a1ca43251946cd63e72bf6ecfdd3ec6d2
ajay <ajaybirsingh21@gmail.com>
2019-05-13 17:16:08 +0530

    Updates

--- ML/tflite.py
+++ ML/tflite.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
 
-converter = tf.lite.TFLiteConverter.from_saved_model('C:/Users/ajayb/OneDrive/Desktop/roj/SM/1557730360',signature_key='predict')
+converter = tf.lite.TFLiteConverter.from_saved_model('C:/Users/ajayb/OneDrive/Desktop/roj/SM/1557746432',signature_key='predict')
 tflite_model = converter.convert()
 #open("converted_model.tflite", "wb").write(tflite_model)
